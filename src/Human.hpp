b0c158f1c8860c4d907927304046ec798526aca5
Raphaeldcp <145991387+Raphaeldcp@users.noreply.github.com>
2024-03-22 14:16:05 +0100

    Update Human.hpp

--- src/Human.hpp
+++ src/Human.hpp
@@ -1,8 +1,33 @@
+#include <iostream>
+#include <string>
+
 class Human {
     public: 
         int speed; 
         bool life; 
         int size; 
         int gravity; 
-        void sprite; 
+        void sprite(); 
 };
+
+class Player : public Human {
+    public:
+        std::string Name; 
+        int jump; 
+        int direction;
+        int maxHeight;
+        int minHeight;
+        void sprite();
+        int initialWidth;
+};
+
+class PoliceMan : public Human {
+    public:
+        int randomDirection;
+        int speed;
+        bool diffrentState; 
+};
+
+void Human::sprite(){
+    
+}
