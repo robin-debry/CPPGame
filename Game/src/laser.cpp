f16502b22302b179d30de1800f082ee6277e9c87
Lucas Aubard <lucas.aubard@algosup.com>
2024-04-04 11:49:46 +0200

    laser colision

--- Game/src/laser.cpp
+++ Game/src/laser.cpp
@@ -1,10 +1,12 @@
 #include "../include/laser.hpp"
 
 Laser::Laser(sf::RenderWindow& window) : player(window), window(window), laserSpeed(600.0f), currentLaserFrame(0), laserTimeElapsed(0.0f), laserTime(0.0f), laserTimeDuration(7.0f), warningTime(0.0f), warningFlag(false) {
+
     loadTextures();
 }
 
-void Laser::loadTextures() {
+void Laser::loadTextures()
+{
     for (int i = 0; i < 3; i++)
     {
         sf::Texture texture;
@@ -25,10 +27,10 @@ void Laser::loadTextures() {
         std::cerr << "Failed to load warning texture." << std::endl;
         exit(EXIT_FAILURE);
     }
-
 }
 
-void Laser::setupScene() {
+void Laser::setupScene()
+{
     laser.setTexture(laserTextures[0]);
     sf::Vector2u windowSize = window.getSize();
     unsigned int windowHeight = windowSize.y;
@@ -37,10 +39,10 @@ void Laser::setupScene() {
     warning.setTexture(warningTexture);
     warning.setPosition(1500.0f, yPosition);
     warning.setScale(0.05f, 0.05f);
-
 }
 
-void Laser::update(sf::Time deltaTime, Player& player) {
+void Laser::update(sf::Time deltaTime, Player &player)
+{
 
     sf::Time DeltaTime = clock.restart();
     float dtSeconds = deltaTime.asSeconds();
@@ -50,26 +52,46 @@ void Laser::update(sf::Time deltaTime, Player& player) {
 
     if (laserTime >= laserTimeDuration)
     {
-      
-    if (laserTimeElapsed >= 1.6f)
-    {
-        currentLaserFrame = (currentLaserFrame + 1) % 3;
-        laser.setTexture(laserTextures[currentLaserFrame]);
-        laserTimeElapsed = 0.0f;
+
+        if (laserTimeElapsed >= 1.6f)
+        {
+            currentLaserFrame = (currentLaserFrame + 1) % 3;
+            laser.setTexture(laserTextures[currentLaserFrame]);
+            laserTimeElapsed = 0.0f;
+        }
+
+        else if (laserTime >= 11.0f)
+        {
+            laserTime = 0.0f;
+            laser.setPosition(0.0f, static_cast<float>(rand() % static_cast<int>(window.getSize().y - laser.getGlobalBounds().height)));
+            warning.setPosition(1500.0f, laser.getPosition().y);
+        }
+
+        sf::Vector2f playerCenter = player.getPosition() + sf::Vector2f(player.getGlobalBounds().width / 2, player.getGlobalBounds().height / 2);
+        sf::Vector2f laserCenter = laser.getPosition() + sf::Vector2f(laser.getGlobalBounds().width / 2, laser.getGlobalBounds().height / 2);
+        float distance = std::sqrt(std::pow(playerCenter.x - laserCenter.x, 2) + std::pow(playerCenter.y - laserCenter.y, 2));
+        float minLaserCollisionDistance = 650.0f;
+
+        //Verifies if the player is colliding with the laser (below)
+        if (distance < minLaserCollisionDistance)
+        {
+            player.isDead = true;
+        }
+
+        //Verifies if the player is colliding with the laser (above)
+        if (player.getPosition().y < laser.getPosition().y + laser.getGlobalBounds().height && player.getPosition().y + player.getGlobalBounds().height > laser.getPosition().y)
+        {
+            if (player.getPosition().x + player.getGlobalBounds().width > laser.getPosition().x && player.getPosition().x < laser.getPosition().x + laser.getGlobalBounds().width)
+            {
+                player.isDead = true;
+            }
+        }
+        
     }
-
-    else if (laserTime >= 11.0f)
-    {
-        laserTime = 0.0f;
-        laser.setPosition(0.0f, static_cast<float>(rand() % static_cast<int>(window.getSize().y - laser.getGlobalBounds().height)));
-        warning.setPosition(1500.0f, laser.getPosition().y);
-    }
-
-
-}
 }
 
-void Laser::drawLaser() {
+void Laser::drawLaser()
+{
     if (laserTime + 2 >= laserTimeDuration)
     {
         window.draw(warning);
