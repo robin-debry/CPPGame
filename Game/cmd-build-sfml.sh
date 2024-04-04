242af936ebc4085f29816abbe6517629bb55642c
Robin Debry <91249812+robin-debry@users.noreply.github.com>
2024-04-04 15:32:51 +0200

    add music

--- Game/cmd-build-sfml.sh
+++ Game/cmd-build-sfml.sh
@@ -1,2 +1,2 @@
-g++ -std=c++11 src/main.cpp src/game.cpp src/background.cpp src/player.cpp src/coin.cpp src/laser.cpp src/obstacle.cpp src/menu.cpp -o game -I/opt/homebrew/Cellar/sfml/2.6.1/include -L/opt/homebrew/Cellar/sfml/2.6.1/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
+g++ -std=c++11 src/main.cpp src/game.cpp src/background.cpp src/player.cpp src/coin.cpp src/laser.cpp src/obstacle.cpp src/menu.cpp src/policeman.cpp -o game -I/opt/homebrew/Cellar/sfml/2.6.1/include -L/opt/homebrew/Cellar/sfml/2.6.1/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 ./game
